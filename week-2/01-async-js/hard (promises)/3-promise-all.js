ce31ed6a7d13e853c3802e3d41f5b4b1ad0f5750
iamvinalsharma <152777568+iamvinalsharma@users.noreply.github.com>
2023-12-19 03:05:24 +0530

    Completed 3-promise-all.js

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -4,20 +4,43 @@
  * Return a promise.all which return the time in milliseconds it takes to complete the entire operation.
  */
 
-function wait1(t) {
+// TESTS: https://github.com/100xdevs-cohort-2/assignments/blob/master/week-2/01-async-js/tests/3-promise-all.test.js
 
-}
-
-function wait2(t) {
-
-}
-
-function wait3(t) {
-
+function wait(t) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve();
+        }, t);
+    });
 }
 
 function calculateTime(t1, t2, t3) {
-
+    let startTime = Date.now();
+    const promise1 = wait(t1*1000);
+    const promise2 = wait(t2*1000);
+    const promise3 = wait(t3*1000);
+    const allPromises = Promise.all([promise1, promise2, promise3]);
+    return allPromises.then(function() {
+        let endTime = Date.now();
+        return (endTime - startTime);
+    });  
 }
 
 module.exports = calculateTime;
+
+// Testing the code:
+
+// bash-3.2$ npx jest ../tests/3-promise-all.test.js 
+//  PASS  ../tests/3-promise-all.test.js (20.278 s)
+//   calculateTime function
+//     ✓ calculates time 1 (3008 ms)
+//     ✓ calculates time 2 (7003 ms)
+//     ✓ calculates time 3 (10003 ms)
+//     ✓ calculates time for zero seconds (2 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       4 passed, 4 total
+// Snapshots:   0 total
+// Time:        20.332 s, estimated 21 s
+// Ran all test suites matching /..\/tests\/3-promise-all.test.js/i.
+// bash-3.2$ 
